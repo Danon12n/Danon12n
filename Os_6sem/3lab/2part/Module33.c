bf2e6eb41f12be7a1d4baceb3a4501e15248ec2a
Даниил Нечитайло <masterwars22@yandex.ru>
2021-11-27 16:05:10 +0300

    что-то по осям и полезное по матстату

--- Os_6sem/3lab/2part/Module33.c
+++ Os_6sem/3lab/2part/Module33.c
@@ -7,17 +7,17 @@ MODULE_AUTHOR("Nechitaylo D.V.");
 
 static int __init Module32_init(void) 
 { 
-   printk("ModuleMsg: Module32 Loaded!\n"); 
+   printk("ModuleMsg: Module33 Loaded!\n"); 
    printk("ModuleMsg: string exported from Module31 : %s\n", Module31_string); 
    printk("ModuleMsg: string returned Module31_proc1() is : %s\n", Module31_proc1()); 
    printk("ModuleMsg: int exported from Module31 : %d\n", Module31_int);
    printk("ModuleMsg: int returned by Module31_proc2 is : %d", Module31_proc2());
-   return 0; 
+   return -1; 
 } 
 
 static void __exit Module32_exit(void) 
 { 
-   printk("ModuleMsg: Module32 Unloaded!\n"); 
+   printk("ModuleMsg: Module33 Unloaded!\n"); 
 } 
 
 module_init(Module32_init);
